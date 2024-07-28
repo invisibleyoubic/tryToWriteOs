c3ab7efa62a465d09591f514f47f5bce0ef76e2d
Daniil <79036954430d@gmail.com>
2024-07-28 18:56:35 +0300

    Experiment with colors

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 int _start()
 {
-    char *video_memory = (char*)0xb8000;
-    *video_memory = 'k';
+    const unsigned short int sizeOfScreen = 2 * 80 * 25;
+    
+    char *screnPtr = (char*)0xb8000;
+
+    unsigned short int itr = 1;
+    unsigned char color = 0;
+
+    while (itr < sizeOfScreen)
+    {
+        *(screnPtr + itr) = color;
+        itr += 2;
+        color++;
+    } 
 }
